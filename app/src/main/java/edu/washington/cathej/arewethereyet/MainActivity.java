9106bcb142c01ab16cb780ad32ae76d3fcb09bcd
Cat <cathej6@gmail.com>
2017-02-18 20:56:18 -0800

    Commiting solution.

---
+++ app/src/main/java/edu/washington/cathej/arewethereyet/MainActivity.java
@@ -0,0 +1,105 @@
+package edu.washington.cathej.arewethereyet;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends Activity {
+
+    private AlarmManager alarmManager;
+    private PendingIntent alarmPendingIntent;
+    private Intent alarmIntent;
+    private boolean isStart = true;
+    private Button startStop;
+    private EditText message;
+    private EditText phoneNumber;
+    private EditText timeInterval;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startStop = (Button) findViewById(R.id.startstop);
+        message = (EditText) findViewById(R.id.message);
+        phoneNumber = (EditText) findViewById(R.id.phonenumber);
+        timeInterval = (EditText) findViewById(R.id.timeinterval);
+
+        //isStart = startStop.getText().toString().equals("Start");
+
+        startStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isStart && !message.getText().toString().equals("") &&
+                        !phoneNumber.getText().toString().equals("") &&
+                        !timeInterval.getText().toString().equals("") &&
+                        Integer.parseInt(timeInterval.getText().toString()) > 0) {
+                    Log.i("debug", "Starting alarm");
+                    alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                    alarmIntent = new Intent(MainActivity.this, TextReceiver.class);
+                    alarmIntent.putExtra("phoneNumber", phoneNumber.getText().toString());
+                    alarmPendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, alarmIntent, 0);
+
+                    alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            SystemClock.elapsedRealtime() + 100,
+                            6000 * Integer.parseInt(timeInterval.getText().toString()),
+                            alarmPendingIntent);
+                    startStop.setText("Stop");
+                    isStart = !isStart;
+                } else if (!isStart) {
+
+                    Intent newIntent = new Intent(MainActivity.this, TextReceiver.class);
+                    PendingIntent newPending = PendingIntent.getBroadcast(MainActivity.this, 0, newIntent, 0);
+                    Log.i("debug", "Canceling alarm");
+                    stopService(newIntent);
+                    alarmManager.cancel(newPending);
+
+                    startStop.setText("Start");
+                    isStart = !isStart;
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        savedInstanceState.putBoolean("isStart", isStart);
+        savedInstanceState.putString("message", message.getText().toString());
+        savedInstanceState.putString("phoneNumber", phoneNumber.getText().toString());
+        savedInstanceState.putString("timeInterval", timeInterval.getText().toString());
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+        isStart = savedInstanceState.getBoolean("isStart");
+        String messageString = savedInstanceState.getString("message");
+        String phoneNumberString = savedInstanceState.getString("phoneNumber");
+        String timeIntervalString = savedInstanceState.getString("timeInterval");
+
+        if (isStart) {
+            startStop.setText("Start");
+        } else {
+            startStop.setText("Stop");
+        }
+
+        message.setText(messageString);
+        phoneNumber.setText(phoneNumberString);
+        timeInterval.setText(timeIntervalString);
+    }
+}
---
+++ app/src/main/java/edu/washington/cathej/arewethereyet/TextReceiver.java
@@ -0,0 +1,24 @@
+package edu.washington.cathej.arewethereyet;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by catherinejohnson on 2/18/17.
+ */
+
+public class TextReceiver extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent) {
+        // This will be where we update the TopicReference with new data
+        // But for now we will just send Toast notifications when it's done.
+
+        Log.i("debug", "message sent to phone number: " + intent.getStringExtra("phoneNumber"));
+        CharSequence text = intent.getStringExtra("phoneNumber") + ": Are we there yet?";
+        Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
